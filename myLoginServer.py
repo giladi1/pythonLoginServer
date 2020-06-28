5c766ef83b4feb98c8d4d677c676f15aa18033d0
ofer <ofer12@gmail.com>
2020-06-28 21:27:43 +0300

    Add files via upload

--- myLoginServer.py
+++ myLoginServer.py
@@ -34,5 +34,5 @@ def index(inP):
     return result
 
 if(__name__== '__main__'):
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port=5000,debug=True)
     #app.run(debug=True, ssl_context='adhoc')
